98c44c8511b849a9ddd27159518096ebc6271794
Young-Jung <youngjung365@gmail.com>
2023-12-19 02:44:04 -0500

    ARP Request and ARP Response

--- network_scanner.py
+++ network_scanner.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python
+
+import scapy.all as scapy
+
+def scan(ip):
+    # ARP Request
+    scapy.arping(ip)
+    # ARP Response: return MAC Address of the IP
+
+# scan("192.168.109.2")
+scan("192.168.109.1/24")
