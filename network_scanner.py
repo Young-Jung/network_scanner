81a8d91a22efe61043d869dd1eeb8445cd4d3fc9
Young-Jung <youngjung365@gmail.com>
2024-01-03 03:07:18 -0500

    combine Frames to Broadcast packets

--- network_scanner.py
+++ network_scanner.py
@@ -6,8 +6,19 @@ def scan(ip):
     # ARP Request
     # scapy.arping(ip)
     arp_request = scapy.ARP(pdst = ip)
-    print(arp_request.summary())
+    # print(arp_request.summary())
+    # scapy.ls(scapy.ARP())
     # ARP Response: return MAC Address of the IP
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    # "ff:ff:ff:ff:ff:ff" is MAC address for Broadcast
+    # scapy.ls(scapy.Ether())
+    # print(broadcast.summary())
+    arp_request_broadcast = broadcast/arp_request
+
+    arp_request.show()
+    broadcast.show()
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
 
 # scan("192.168.109.2")
 scan("192.168.109.2/24")
